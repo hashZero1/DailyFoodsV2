dcdb964e10cd2256235fc32752c04c769b44a9a7
Animesh Sharma <finaldestination8491@gmail.com>
2025-03-08 15:38:36 +0530

    update details page

--- src/components/home/DetailsPageComponent.jsx
+++ src/components/home/DetailsPageComponent.jsx
@@ -1,9 +1,13 @@
 import { Drawer } from "vaul";
 
 export default function DetailsPageComponent({ recipe }) {
-  const nameless = () => {
+  const Nameless = () => {
     const anslyzedIns = recipe.analyzedInstructions[0]?.steps;
-    return anslyzedIns?.map((dt) => <li key={dt.number}>{dt.step}</li>);
+    return anslyzedIns?.map((dt) => (
+      <li className="ml-5" key={dt.number}>
+        {dt.step}
+      </li>
+    ));
   };
 
   return (
@@ -14,55 +18,63 @@ export default function DetailsPageComponent({ recipe }) {
         </button>
       </Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+        <Drawer.Overlay className="fixed inset-0 bg-black/50" />
         <Drawer.Content className="bg-zinc-100 z-30 text-gray-700 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
           <div className="pt-2 lg:p-4 bg-white overflow-y-scroll no-scrollbar rounded-t-[10px] flex-1">
-            <div className="mx-auto w-14 h-1.5 shrink-0 rounded-full bg-zinc-300 mb-8" />
-            <div className="flex flex-col bg-gray-100 w-11/12 justify-center p-4 lg:p-10 mx-auto rounded-t-[10px]">
-              <Drawer.Title className="font-bold text-xl lg:text-4xl mb-5  bg-white rounded-[12px]  p-4">
-                {recipe.title}
-              </Drawer.Title>
-              <div className="flex flex-col lg:flex-row justify-between lg:text-lg lg:w-3/5">
-                <img
-                  className="rounded-[10px]"
-                  src={recipe.image}
-                  alt={recipe.title}
-                />
-                <div className="my-4 lg:my-0">
-                  <b className=" mt-5">Total servings</b>
-                  <p>{recipe.servings}</p>
+            <div className="mx-auto w-24 h-1.5 shrink-0 rounded-full bg-red-400 mb-8 cursor-grab" />
+            <div className="flex flex-col justify-center p-4 lg:p-10 max-w-[90rem] mx-auto rounded-t-[10px]">
+              <div className="flex flex-col lg:flex-row  lg:text-lg ">
+                <div className="lg:mr-10 mb-5 lg:mb-0 ">
+                  <img
+                    className="w-full rounded-xl"
+                    src={recipe.image}
+                    alt={recipe.title}
+                  />
                 </div>
-                <div className="capitalize">
-                  <b>DishTypes</b>
-                  <p>
-                    {recipe.dishTypes[0]}
-                    <br />
-                    {recipe.dishTypes[1]}
-                    <br />
-                    {recipe.dishTypes[2]}
-                    <br />
-                    {recipe.dishTypes[3]}
-                    <br />
-                    {recipe.dishTypes[4]}
-                  </p>
+                <div>
+                  <Drawer.Title className="font-bold text-xl lg:text-4xl text-gray-800">
+                    {recipe.title}
+                  </Drawer.Title>
+                  <div className="flex mt-6">
+                    <div className="text-xl text-gray-700 font-semibold mr-10">
+                      <p>Total servings</p>
+                      <p className="text-green-100 mt-2 font-bold bg-green-500 w-12 h-12 flex items-center justify-center rounded-full">
+                        {recipe.servings}
+                      </p>
+                    </div>
+                    <div className=" text-gray-700 capitalize">
+                      <p className="text-xl font-semibold">DishTypes</p>
+                      <div className="">
+                        <p>{recipe.dishTypes[0]}</p>
+                        <p>{recipe.dishTypes[1]}</p>
+                        <p>{recipe.dishTypes[2]}</p>
+                        <p>{recipe.dishTypes[3]}</p>
+                        <p>{recipe.dishTypes[4]}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="lg:w-5/6">
-                <h2 className="font-semibold mb-2 text-xl lg:text-2xl mt-5 lg:mt-8">
+              <div className=" text-justify">
+                <h2 className="font-black text-gray-400 mb-2 text-xl lg:text-4xl mt-5 lg:mt-8">
                   Instructions :
                 </h2>
-                <p className=" leading-relaxed tracking-wide lg:text-lg">
-                  {!recipe.instructions ? "Not Available" : recipe.instructions}{" "}
+                <p className=" leading-relaxed tracking-wide lg:text-lg ">
+                  {!recipe.instructions ? "Not Available" : recipe.instructions}
                 </p>
-                <h3 className="mb-2  mt-8 text-lg lg:text-xl font-semibold">
+                <h3 className="mb-2 font-black text-gray-400 mt-8 text-lg lg:text-2xl">
                   Step by Steps Intructions :
                 </h3>
-                <p className="leading-relaxed tracking-wide lg:text-lg">
-                  {!recipe.analyzedInstructions ? "Not Available" : nameless()}{" "}
-                </p>
+                <ul className="leading-relaxed tracking-wide lg:text-lg list-disc ">
+                  {!recipe.analyzedInstructions ? (
+                    "Not Available"
+                  ) : (
+                    <Nameless />
+                  )}
+                </ul>
               </div>
             </div>
-            <div className="mx-auto w-40 h-1.5 shrink-0 rounded-full bg-zinc-300  my-2" />
+            {/* <div className="mx-auto w-40 h-1.5 shrink-0 rounded-full bg-zinc-300  my-2" /> */}
           </div>
         </Drawer.Content>
       </Drawer.Portal>
--- src/components/skeletons/ErrorScreen.jsx
+++ src/components/skeletons/ErrorScreen.jsx
@@ -5,7 +5,7 @@ const ErrorScreen = () => {
   return (
     <div className="h-[50%] w-full mt-20 p-2">
       <h1 className="text-3xl lg:text-5xl my-20 text-center text-gray-300 font-black ">
-        API seem to be exhausted try again tomorrow
+        API seem to be exhausted! Try again tomorrow
       </h1>
       <Lottie
         className="w-[90%] lg:w-[60%] 2xl:w-[80%] mx-auto"
